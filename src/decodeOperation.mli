327993dc16b5b39168ca21cff8103e2b252a97ee
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-02-28 09:10:12 +0900

    use 'include module type of ...'

--- src/decodeOperation.mli
+++ src/decodeOperation.mli
@@ -3,45 +3,7 @@ open Basic
 open Value
 open DecodeBasic
 
-type 'a decoder
-val return : 'a -> 'a decoder
-val err : Error.t -> 'a decoder
-val ( >>= ) : 'a decoder -> ('a -> 'b decoder) -> 'b decoder
-val transform_result : 'a ok -> 'a decoder
-val mapM : ('a -> 'b decoder) -> 'a list -> ('b list) decoder
-
-(** [run core offset dec] starts to decode the source [core] at the position [offset]
-    by using [dec] and returns the resulting value. *)
-val run : common_source_core -> offset -> 'a decoder -> 'a ok
-
-(** Returns the current position. *)
-val current : offset decoder
-
-(** [seek offset] jumps to the position [offset]. *)
-val seek : offset -> unit decoder
-
-(** Advances the position by the given number of bytes. *)
-val d_skip : int -> unit decoder
-
-(** Reads a byte as an unsigned integer. *)
-val d_uint8 : int decoder
-
-(** Reads a byte as a signed integer. *)
-val d_int8 : int decoder
-
-(** Reads 2 bytes as an unsigned integer. *)
-val d_uint16 : int decoder
-
-(** Reads 2 bytes as a signed integer. *)
-val d_int16 : int decoder
-
-(** Reads 4 bytes as an unsigned integer.
-    Due to the limitation of the range representable by the type [int] under 32-bit environments,
-    this function returns [wint]-typed values. *)
-val d_uint32 : wint decoder
-
-(** Reads 8 bytes as a Unix timestamp. *)
-val d_timestamp : timestamp decoder
+include module type of DecodeOperationCore
 
 (** Same as [d_uint32], except that return values are of type [int].
     Strictly speaking, this function is not valid under 32-bit environments. *)
--- src/decodeOperationCore.mli
+++ src/decodeOperationCore.mli
@@ -6,19 +6,54 @@ open Basic
 open Value
 open DecodeBasic
 
+(** The type for decoders by running which one can gain a value of type ['a]. *)
 type 'a decoder
+
+(** The monadic [pure] function of decoders.
+    [return v] makes a decoder that always produces [v] without moving its position. *)
 val return : 'a -> 'a decoder
+
+(** [err e] makes a decoder that always fails with reason [e]. *)
 val err : Error.t -> 'a decoder
+
+(** The monadic binding operator for decoders. *)
 val ( >>= ) : 'a decoder -> ('a -> 'b decoder) -> 'b decoder
+
+(** [transform_result (Ok(v))] is equivalent to [return v], while [transform_result (Error(e))] is to [err e]. *)
 val transform_result : 'a ok -> 'a decoder
+
+(** The monadic list-mapping function for decoders. *)
 val mapM : ('a -> 'b decoder) -> 'a list -> ('b list) decoder
-val run : common_source_core -> int -> 'a decoder -> 'a ok
+
+(** [run core offset dec] starts to decode the source [core] at the position [offset]
+    by using [dec] and returns the resulting value. *)
+val run : common_source_core -> offset -> 'a decoder -> 'a ok
+
+(** Returns the current position. *)
 val current : offset decoder
+
+(** [seek offset] jumps to the position [offset]. *)
 val seek : offset -> unit decoder
+
+(** Advances the position by the given number of bytes. *)
 val d_skip : int -> unit decoder
+
+(** Reads a byte as an unsigned integer. *)
 val d_uint8 : int decoder
+
+(** Reads a byte as a signed integer. *)
 val d_int8 : int decoder
+
+(** Reads 2 bytes as an unsigned integer. *)
 val d_uint16 : int decoder
+
+(** Reads 2 bytes as a signed integer. *)
 val d_int16 : int decoder
+
+(** Reads 4 bytes as an unsigned integer.
+    Due to the limitation of the range representable by the type [int] under 32-bit environments,
+    this function returns [wint]-typed values. *)
 val d_uint32 : wint decoder
+
+(** Reads 8 bytes as a Unix timestamp. *)
 val d_timestamp : timestamp decoder

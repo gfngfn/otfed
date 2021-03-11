1cd6e6d3469f832c80365eb368457616cbb2aeba
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-03-12 01:44:45 +0900

    separate 'DecodeTtf' from 'Decode'

---
+++ src/decodeTtf.mli
@@ -0,0 +1,12 @@
+
+open Value
+open DecodeBasic
+open DecodeOperation
+
+val loca : ttf_source -> glyph_id -> (ttf_glyph_location option) ok
+
+val d_glyf : (ttf_glyph_description * bounding_box) decoder
+
+val glyf : ttf_source -> ttf_glyph_location -> (ttf_glyph_description * bounding_box) ok
+
+val path_of_ttf_contour : ttf_contour -> quadratic_path ok

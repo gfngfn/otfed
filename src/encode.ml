246f2e6257203428ca1917366407fe6cdaf7f9b4
gfngfn <suwa-takashi141@g.ecc.u-tokyo.ac.jp>
2021-04-10 06:19:47 +0900

    separate 'Encode{Ttf,Cff}Maxp' from 'Encode'

--- src/encode.ml
+++ src/encode.ml
@@ -4,16 +4,10 @@ open Value
 open EncodeOperation.Open
 
 
-module Error = EncodeError
+include EncodeBasic
+
+
 module Subset = EncodeSubset
-
-
-type 'a ok = ('a, Error.t) result
-
-type table = {
-  tag      : Value.Tag.t;
-  contents : string;
-}
 
 
 module Head = struct
@@ -356,60 +350,13 @@ end
 
 module Ttf = struct
 
-  module Maxp = struct
-
-    let make (imaxp : Intermediate.Ttf.Maxp.t) : table ok =
-      let table_version = !%% 0x00010000L in
-      let enc =
-        let open EncodeOperation in
-        e_uint32 table_version                  >>= fun () ->
-        e_uint16 imaxp.num_glyphs               >>= fun () ->
-        e_uint16 imaxp.max_points               >>= fun () ->
-        e_uint16 imaxp.max_contours             >>= fun () ->
-        e_uint16 imaxp.max_composite_points     >>= fun () ->
-        e_uint16 imaxp.max_composite_contours   >>= fun () ->
-        e_uint16 imaxp.max_zones                >>= fun () ->
-        e_uint16 imaxp.max_twilight_points      >>= fun () ->
-        e_uint16 imaxp.max_storage              >>= fun () ->
-        e_uint16 imaxp.max_function_defs        >>= fun () ->
-        e_uint16 imaxp.max_instruction_defs     >>= fun () ->
-        e_uint16 imaxp.max_stack_elements       >>= fun () ->
-        e_uint16 imaxp.max_size_of_instructions >>= fun () ->
-        e_uint16 imaxp.max_component_elements   >>= fun () ->
-        e_uint16 imaxp.max_component_depth      >>= fun () ->
-        return ()
-      in
-      let open ResultMonad in
-      enc |> EncodeOperation.run >>= fun (contents, ()) ->
-      return {
-        tag = Value.Tag.table_maxp;
-        contents;
-      }
-
-  end
+  module Maxp = EncodeTtfMaxp
 
 end
 
 
 module Cff = struct
 
-  module Maxp = struct
-
-    let make (imaxp : Intermediate.Cff.Maxp.t) : table ok =
-      let table_version = !%% 0x00005000L in
-      let enc =
-        let open EncodeOperation in
-        e_uint32 table_version    >>= fun () ->
-        e_uint16 imaxp.num_glyphs >>= fun () ->
-        return ()
-      in
-      let open ResultMonad in
-      enc |> EncodeOperation.run >>= fun (contents, ()) ->
-      return {
-        tag = Value.Tag.table_maxp;
-        contents;
-      }
-
-  end
+  module Maxp = EncodeCffMaxp
 
 end
--- src/encodeBasic.ml
+++ src/encodeBasic.ml
@@ -1,2 +1,9 @@
 
 module Error = EncodeError
+
+type 'a ok = ('a, Error.t) result
+
+type table = {
+  tag      : Value.Tag.t;
+  contents : string;
+}
